1b5e41587ae65b5398fcd3b888cd65da35101a36
lijippy@163.com <lijippy@163.com>
2020-04-18 00:12:10 +0800

    update carbin-cmake

--- cmake/carbin_module.cmake
+++ cmake/carbin_module.cmake
@@ -1,12 +1,32 @@
-include(project_profile)
 include(carbin_error)
 include(carbin_debug)
+
+if (NOT CARBIN_PREFIX)
+    set(CARBIN_PREFIX ${PROJECT_SOURCE_DIR}/carbin)
+    carbin_print("CARBIN_PREFIX not set, set to ${CARBIN_PREFIX}")
+endif ()
+
+link_directories(${CARBIN_PREFIX}/lib)
+carbin_print("add link dir ${CARBIN_PREFIX}")
+include_directories(${CARBIN_PREFIX}/include)
+carbin_print("add include dir ${CARBIN_PREFIX}")
+include_directories(${PROJECT_SOURCE_DIR})
+carbin_print("add include dir ${PROJECT_SOURCE_DIR}")
+include_directories(${PROJECT_BINARY_DIR})
+carbin_print("add include dir ${PROJECT_BINARY_DIR}")
+
+
+include(project_profile)
 include(carbin_cxx_flags)
 include(carbin_outof_source)
 include(carbin_platform)
 include(carbin_pkg_dump)
 
 CARBIN_ENSURE_OUT_OF_SOURCE_BUILD("must out of source dir")
+
+if (NOT DEV_MODE AND ${CMAKE_PROJECT_VERSION} MATCHES "0.0.0")
+    carbin_error("CMAKE_PROJECT_VERSION must be set in file project_profile")
+endif()
 
 include(CheckCXXCompilerFlag)
 CHECK_CXX_COMPILER_FLAG(${CARBIN_STD} COMPILER_SUPPORTS_CARBIN_STD)
