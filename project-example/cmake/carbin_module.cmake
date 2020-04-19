50c3e53da08832332b14e492a47b9146196cbacc
lijippy@163.com <lijippy@163.com>
2020-04-19 21:46:54 +0800

    update carbin cmake

--- project-example/cmake/carbin_module.cmake
+++ project-example/cmake/carbin_module.cmake
@@ -3,7 +3,6 @@ include(carbin_debug)
 include(carbin_cc_library)
 include(carbin_cc_test)
 include(carbin_cc_binary)
-include(carbin_cc_benchmark)
 include(carbin_check)
 
 if (NOT CARBIN_PREFIX)
@@ -11,32 +10,11 @@ if (NOT CARBIN_PREFIX)
     carbin_print("CARBIN_PREFIX not set, set to ${CARBIN_PREFIX}")
 endif ()
 
-link_directories(${CARBIN_PREFIX}/${CARBIN_INSTALL_LIBDIR})
-include_directories(${CARBIN_PREFIX}/${CARBIN_INSTALL_INCLUDEDIR})
 include_directories(${PROJECT_SOURCE_DIR})
 carbin_print("add include dir ${PROJECT_SOURCE_DIR}")
 include_directories(${PROJECT_BINARY_DIR})
 carbin_print("add include dir ${PROJECT_BINARY_DIR}")
 list(APPEND CMAKE_PREFIX_PATH ${CARBIN_PREFIX})
-
-MACRO(directory_list result curdir)
-    FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
-    SET(dirlist "")
-    FOREACH(child ${children})
-        IF(IS_DIRECTORY ${curdir}/${child} )
-            LIST(APPEND dirlist ${child})
-        ENDIF()
-    ENDFOREACH()
-    SET(${result} ${dirlist})
-ENDMACRO()
-
-directory_list(install_libs "${CARBIN_PREFIX}/${CARBIN_INSTALL_LIBDIR}/cmake")
-
-FOREACH(installed_lib  ${install_libs})
-    list(APPEND CMAKE_MODULE_PATH
-            ${CARBIN_PREFIX}/${CARBIN_INSTALL_LIBDIR}/cmake/${installed_lib})
-
-ENDFOREACH()
 
 include(project_profile)
 include(carbin_outof_source)
--- project-example/cmake/project_profile.cmake
+++ project-example/cmake/project_profile.cmake
@@ -1,7 +1,7 @@
 
 set(PROJECT_DESCRIPTION "carbin c++ lib")
-set(PROJECT_VERSION_MAJOR 1)
-set(PROJECT_VERSION_MINOR 2)
+set(PROJECT_VERSION_MAJOR 0)
+set(PROJECT_VERSION_MINOR 0)
 set(PROJECT_VERSION_PATCH 0)
 set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
 
@@ -10,14 +10,11 @@ option(CARBIN_STATUS_DEBUG "cmake toolchain debug info" ON)
 
 option(ENABLE_TESTING "enable unit test" ON)
 option(CARBIN_PACKAGE_GEN "enable package gen" ON)
-option(ENABLE_BENCHMARK "enable benchmark" ON)
+option(ENABLE_BENCHMARK "enable benchmark" OFF)
 option(ENABLE_EXAMPLE "enable benchmark" ON)
 
-if (ENABLE_TESTING)
-    set(CARBIN_RUN_TESTS true)
-endif ()
 
-set(CMAKE_VERBOSE_MAKEFILE OFF)
+set(CMAKE_VERBOSE_MAKEFILE ON)
 
 set(CMAKE_MACOSX_RPATH 1)
 
