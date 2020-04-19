ce3fc595aed818f470b41ffe34c600ebb8ac537e
lijippy@163.com <lijippy@163.com>
2020-04-19 21:53:21 +0800

    update cmake

--- cmake/project_profile.cmake
+++ cmake/project_profile.cmake
@@ -9,9 +9,9 @@ option(CARBIN_STATUS_PRINT "cmake toolchain print" ON)
 option(CARBIN_STATUS_DEBUG "cmake toolchain debug info" ON)
 
 option(ENABLE_TESTING "enable unit test" ON)
-option(CARBIN_PACKAGE_GEN "enable package gen" ON)
-option(ENABLE_BENCHMARK "enable benchmark" ON)
-option(ENABLE_EXAMPLE "enable benchmark" ON)
+option(CARBIN_PACKAGE_GEN "enable package gen" OFF)
+option(ENABLE_BENCHMARK "enable benchmark" OFF)
+option(ENABLE_EXAMPLE "enable benchmark" OFF)
 
 if (ENABLE_TESTING)
     set(CARBIN_RUN_TESTS true)
